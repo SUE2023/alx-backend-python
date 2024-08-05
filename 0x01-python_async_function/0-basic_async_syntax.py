9f643bc4fd53427dd81c1149b978dfd86b9da65d
Susan <susansewe@gmail.com>
2024-08-05 17:55:57 +0000

    Task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,16 +1,13 @@
-import random
+#!/usr/bin/env python3
+'''Task 0's module.
+'''
 import asyncio
+import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Asynchronous coroutine that waits for a random delay."""
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
-
-if __name__ == "__main__":
-    random.seed(444)
-
-    # Manually create an event loop
-    loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(wait_random())
-    print(f"Waited for: {result} seconds")
+    '''Waits for a random number of seconds.
+    '''
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
