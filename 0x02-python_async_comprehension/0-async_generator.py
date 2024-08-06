e131a9a3585ef201998943bb7a7e7e218d50810e
Susan <susansewe@gmail.com>
2024-08-06 12:59:36 +0000

    Task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-"""Async coroutine"""
+"""Async coroutine to generate random numbers"""
 import random
 import asyncio
-from typing import List
+from typing import AsyncGenerator
 
 
-async def async_generator() -> List[float]:
-    """Generates a sequence of 10 numbers."""
-    numbers = []
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Generates a sequence of 10 random numbers, waiting 1 second."""
     for _ in range(10):
         await asyncio.sleep(1)
-        numbers.append(random.random() * 10)
-    return numbers
+        yield random.uniform(0, 10)
