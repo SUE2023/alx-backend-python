8382a3ee9655dcdfde72a6e427fed22b7770caac
Susan <susansewe@gmail.com>
2024-08-24 19:35:52 +0000

    Task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch
 
 
@@ -52,6 +52,37 @@ class TestGetJson(unittest.TestCase):
 
             # Ensure get method was called exactly once with the correct URL
             mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Class to rest memoization function"""
+    def test_memoize():
+        """ Test that on calling a_property twice, the correct result is
+        returned but a_method is only called once by assert_called_once.
+        """
+
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+        @memoize
+        def a_property(self):
+            return self.a_method()
+
+        # with patch.object(TestClass, 'a_method') as mock:
+        #  test_class = TestClass()
+        #  test_class.a_property()
+        #  test_class.a_property()
+        #  mock.assert_called_once()
+        with patch.object(TestClass, 'a_method', return_value=42) \
+                as mock_method:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property, 42)
+            # First call, should invoke a_method
+            self.assertEqual(test_class.a_property, 42)
+            # Second call, should not invoke a_method
+            mock_method.assert_called_once()
 
 
 if __name__ == "__main__":
